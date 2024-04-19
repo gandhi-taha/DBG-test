e0688209d786b10b27ab548fb49cff0f75526b16
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2024-04-19 14:42:35 +0200

    latest version
    19/04/2024 - 14:42

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import joblib
 from typing import Literal
 from dataclasses import dataclass
 import json
+from random import randint
 
 st.set_page_config(layout='wide', page_title="Natina-AI", page_icon="DBG-Logo.png")
 # custom_html = """
@@ -61,27 +62,41 @@ def response_generator():
             yield respone
             time.sleep(0.05)
 
+def on_click_callback():
+	user_prompt = st.session_state.user_prompt
+	st.session_state.history.append(user_prompt)
+
+
+avatars = {
+    "user": "static/user.png:",
+    "ai": "/static/chatbot.png",}
 
 # Sidebar content 
 st.sidebar.image("DBG-Logo.png", use_column_width=True)
 st.sidebar.header("DBGenAI - Platform")
 sideb = st.sidebar
 Bar1 = sideb.write("-------")
-button1 = sideb.button(" 📁  HR-Bot")
-button2 = sideb.button(" 💻  IT-Bot")
-button3 = sideb.button(" 📈  Finance-Bot")
-Bar2 = sideb.write("-------")
-Text1 = sideb.write(
-	"""
-	#### Natina is currently supporting HR & IT Operations Regulations and Finance Reports.
-	"""
-)
-Bar2 = sideb.write("-------")
-Text2 = sideb.write(
-	"""
-		Copyright © 2024 Deutsche Börse Group - All Rights Reserved.
-	"""
-)
+selectionbox = sideb.selectbox(
+	"Select a Department",
+	("📁 HR Persona", "💻 IT Persona", "📈 Finance Persona"),
+	)
+# button1 = sideb.button(" 📁  HR-Bot")
+# button2 = sideb.button(" 💻  IT-Bot")
+# button3 = sideb.button(" 📈  Finance-Bot")
+# Bar2 = sideb.write("-------")
+# Text1 = sideb.write(
+# 	"""
+# 	#### Natina is currently supporting HR & IT Operations Regulations and Finance Reports.
+# 	"""
+# )
+# Bar2 = sideb.write("-------")
+# Text2 = sideb.write(
+# 	"""
+# 		Copyright © 2024 Deutsche Börse Group - All Rights Reserved.
+# 	"""
+# )
+
+
 
 # Main content
 st.title("Natina-AI")
@@ -95,13 +110,69 @@ if "widget_key" not in st.session_state:
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.greetings = False
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-uploaded_files = st.file_uploader("Choose PPT or PDF file", accept_multiple_files=True, type=[
-                                  "pdf"], key=st.session_state.widget_key)
+
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+uploaded_files = st.file_uploader("Choose PPT or PDF file", accept_multiple_files=True, type=["pdf"], key=st.session_state.widget_key)
+
+
+
+# # Greet user
+if not st.session_state.greetings:
+	with st.chat_message("ai", avatar="static/chatbot.png"):
+		intro = """Hello, I am Natina."""
+		st.markdown(intro)
+		#Add Bot response to chat History
+		# st.session_state.messages.append(intro)
+		# st.session_state.greetings = True
+
+
+# Example prompts
+example_prompts = [
+	"How to access Jira",
+	"Explain me the different Personas",
+	"Use the prompt correcty",
+	"White card with protection from black",
+	"The famous 'Black Lotus' card",
+	"Wizard card with Vigiliance ability",
+]
+
+example_prompts_help = [
+	"Look for a specific card effect",
+	"Search for card type: 'Vampires', card color: 'black', and ability: 'flying'",
+	"Color cards and card type",
+	"Specifc card effect to another mana color",
+	"Search for card names",
+	"Search for card types with specific abilities",
+]
+
+button_cols = st.columns(3)
+button_cols_2 = st.columns(3)
+
+button_pressed = ""
+
+if button_cols[0].button(example_prompts[0], help=example_prompts_help[0]):
+	button_pressed = example_prompts[0]
+elif button_cols[1].button(example_prompts[1], help=example_prompts_help[1]):
+	button_pressed = example_prompts[1]
+elif button_cols[2].button(example_prompts[2], help=example_prompts_help[2]):
+	button_pressed = example_prompts[2]
+
+elif button_cols_2[0].button(example_prompts[3], help=example_prompts_help[3]):
+	button_pressed = example_prompts[3]
+elif button_cols_2[1].button(example_prompts[4], help=example_prompts_help[4]):
+	button_pressed = example_prompts[4]
+elif button_cols_2[2].button(example_prompts[5], help=example_prompts_help[5]):
+	button_pressed = example_prompts[5]
+
+
+
+
 
 # Accept user input
 if prompt := st.chat_input("Type a message"):
@@ -117,10 +188,10 @@ if prompt := st.chat_input("Type a message"):
     st.session_state.messages.append(
         {"role": "user", "content": new_files+"\n"+prompt})
     # Display user message in chat message container
-    with st.chat_message("human"):
+    with st.chat_message("human", avatar="static/user.png"):
         st.markdown(prompt)
     # Display assistant response in chat message container
-    with st.chat_message("ai"):
+    with st.chat_message("ai", avatar="static/chatbot.png"):
         with st.spinner('Processing'):
             response = st.write_stream(response_generator())
         # response=response_generator()
--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,6 @@ import json
 
 st.set_page_config(layout='wide', page_title="Natina-AI", page_icon="DBG-Logo.png")
 
-@dataclass
-class Message:
-	"""Class for keeping track of chat message."""
-	origin: Literal["user", "ai"]
-	message: str
 
 # Streamed response emulator
 # def response_generator():
@@ -34,19 +29,19 @@ def load_css():
 		css = f'<style>{f.read()}</style>'
 		st.markdown(css, unsafe_allow_html=True)
 
-def load_lottiefile(filepath: str):
+# def load_lottiefile(filepath: str):
 
-	'''Load lottie animation file'''
+# 	'''Load lottie animation file'''
 
-	with open(filepath, "r") as f:
-		return json.load(f)
+# 	with open(filepath, "r") as f:
+# 		return json.load(f)
 
-st_lottie(load_lottiefile("images/Nationa-Ai.json"), speed=1, reverse=False, loop=True, quality="high", height=300)
-
+# animation = sideb.st_lottie(load_lottiefile("images/Nationa-Ai.json"), speed=1, reverse=False, loop=False, quality="high", height=300)
 
 def initialize_session_state():
 	if "history" not in st.session_state:
 		st.session_state.history = []
+		st.session_state.greetings = False
 
 def on_click_callback():
 	user_prompt = st.session_state.user_prompt
@@ -72,23 +67,79 @@ st.sidebar.image("DBG-Logo.png", use_column_width=True)
 st.sidebar.header("DBGenAI - Platform")
 sideb = st.sidebar
 Bar1 = sideb.write("-------")
-button1 = sideb.button(" 📁  HR-Bot")
-button2 = sideb.button(" 💻  IT-Bot")
-button3 = sideb.button(" 📈  Finance-Bot")
-Bar2 = sideb.write("-------")
-Text1 = sideb.write(
-	"""
-	#### Natina is currently supporting HR & IT Operations Regulations and Finance Reports.
-	"""
-)
-Bar2 = sideb.write("-------")
-Text2 = sideb.write(
-	"""
-		Copyright © 2024 Deutsche Börse Group - All Rights Reserved.
-	"""
-)
+selectionbox = sideb.selectbox(
+	"Select a Department",
+	("📁 HR Persona", "💻 IT Persona", "📈 Finance Persona"),
+	)
+#selectText = sideb.write("You selected: ", selectionbox)
+# button1 = sideb.button(" 📁  HR Manager")
+# button2 = sideb.button(" 💻  IT Manager")
+# button3 = sideb.button(" 📈  Financel Manager")
+# Bar2 = sideb.write("-------")
+# Text1 = sideb.write(
+# 	"""
+# 	#### Natina is currently supporting HR & IT Operations Regulations and Finance Reports.
+# 	"""
+# )
+# Bar2 = sideb.write("-------")
+# Text2 = sideb.write(
+# 	"""
+# 		Copyright © 2024 Deutsche Börse Group - All Rights Reserved.
+# 	"""
+# )
 
 # Main content
+
+
+# # Greet user
+if not st.session_state.greetings:
+	with st.chat_message("ai"):
+		intro = "Hello, I am Natina. Please let me know what I can do for you."
+		st.markdown(intro)
+		#Add Bot response to chat History
+		st.session_state.history.append(intro)
+		st.session_state.greetings = True
+
+
+# Example prompts
+example_prompts = [
+	"How to access Jira",
+	"Explain me the different Personas",
+	"Use the prompt correcty",
+	"White card with protection from black",
+	"The famous 'Black Lotus' card",
+	"Wizard card with Vigiliance ability",
+]
+
+example_prompts_help = [
+	"Look for a specific card effect",
+	"Search for card type: 'Vampires', card color: 'black', and ability: 'flying'",
+	"Color cards and card type",
+	"Specifc card effect to another mana color",
+	"Search for card names",
+	"Search for card types with specific abilities",
+]
+
+button_cols = st.columns(3)
+button_cols_2 = st.columns(3)
+
+button_pressed = ""
+
+if button_cols[0].button(example_prompts[0], help=example_prompts_help[0]):
+	button_pressed = example_prompts[0]
+elif button_cols[1].button(example_prompts[1], help=example_prompts_help[1]):
+	button_pressed = example_prompts[1]
+elif button_cols[2].button(example_prompts[2], help=example_prompts_help[2]):
+	button_pressed = example_prompts[2]
+
+elif button_cols_2[0].button(example_prompts[3], help=example_prompts_help[3]):
+	button_pressed = example_prompts[3]
+elif button_cols_2[1].button(example_prompts[4], help=example_prompts_help[4]):
+	button_pressed = example_prompts[4]
+elif button_cols_2[2].button(example_prompts[5], help=example_prompts_help[5]):
+	button_pressed = example_prompts[5]
+
+#Prompt
 chat_placeholder = st.container()
 prompt_placeholder = st.form("chat-form")
 placeholder = st.empty()
@@ -116,4 +167,9 @@ with prompt_placeholder:
 		on_click = on_click_callback,
 	)
 
+	cols[0].form_submit_button(
+		"⬆ Upload",
+		type= "primary",
+		on_click = on_click_callback,
+	)
 
